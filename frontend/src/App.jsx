13ac7547b79aec92db8085ec906b0a677b324fb1
zeina <elshemerlyz@gmail.com>
2025-05-10 17:56:52 +0300

    feat: add CoursesList for viewing courses in my major

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
+// frontend/src/App.jsx
+
 import React from 'react';
 import InternshipList from './components/InternshipList';
-
 import CoursesList    from './components/CoursesList';
-function App() {
-  return <InternshipList />;
-}
-
 
 function App() {
   return (
@@ -15,8 +12,5 @@ function App() {
     </>
   );
 }
-
-
-
 
 export default App;
